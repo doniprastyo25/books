c72cd3c6866d054b3f981037afdb169e01f6a19d
doniprastyo25 <donismsbanking@gmail.com>
2022-06-13 16:00:58 +0700

    python firts app from tutorial

---
+++ book_api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from book_api.models import Book
+from book_api.serializer import BookSerializer
+
+# Create your views here.
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all() # Complex data
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
