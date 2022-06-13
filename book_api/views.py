f8c0ed084de5fe6db79bcaf1e2157fc4139a7471
doniprastyo25 <doniprastyo1999@gmail.com>
2022-06-14 05:21:51 +0700

    uodate learn

--- book_api/views.py
+++ book_api/views.py
@@ -10,3 +10,13 @@ def book_list(request):
     books = Book.objects.all() # Complex data
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def book_create(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.data)
+
