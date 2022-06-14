9b5c60ed85099bac1c6bcba6df1f12a773f0ca83
doniprastyo25 <doniprastyo1999@gmail.com>
2022-06-15 06:45:23 +0700

    uodate learn

--- book_api/views.py
+++ book_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from book_api.models import Book
 from book_api.serializer import BookSerializer
 
@@ -18,5 +19,28 @@ def book_create(request):
         serializer.save()
         return Response(serializer.data)
     else:
-        return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def book(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except:
+        return Response({
+            'error':'Book Does Not Exist'
+        }, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+    
+    
+    if request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
