95b682702af1d9728e4103f35a29fe4766595617
doniprastyo25 <doniprastyo1999@gmail.com>
2022-06-15 07:02:30 +0700

    uodate learn

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from book_api.views import book_list, book_create, book
+# from book_api.views import book_list, book_create, book
+from book_api.views import BookList, BookCreate
 
 urlpatterns = [
-    path('', book_create),
-    path('list/', book_list),
-    path('<int:pk>', book)
+    # path('', book_create),
+    path('', BookCreate.as_view()),
+    path('list/', BookList.as_view()),
+    # path('<int:pk>', book)
 ]
